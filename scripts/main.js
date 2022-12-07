740a4f257f7c31fe4d8ac14fc0d017a8c2f247c0
Danique Lammertink <danique.lammertink@hva.nl>
2022-12-07 21:58:32 +0100

    ⚡️Started

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Our bundler automatically creates styling when imported in the main JS file!
+import '../styles/style.css'
+import '../scripts/quiz.js'
+
+// We can use node_modules directely in the browser!
+import * as d3 from 'd3';
+
+console.log('Hello, world!');
---
+++ scripts/quiz.js
@@ -0,0 +1,13 @@
+// // Vragen
+// const vraag1 = document.querySelector(".step1")
+// const vraag2 = document.querySelector(".step2")
+// const vraag3 = document.querySelector(".step3")
+// const vraag4 = document.querySelector(".step4")
+// const vraag5 = document.querySelector(".step5")
+
+// // Antwoorden
+// const vraag1 = document.querySelector(".step1")
+// const vraag2 = document.querySelector(".step2")
+// const vraag3 = document.querySelector(".step3")
+// const vraag4 = document.querySelector(".step4")
+// const vraag5 = document.querySelector(".step5")
